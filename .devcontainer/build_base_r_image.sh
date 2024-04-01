b13a8989327594cefd5d2f4f1dc5e504fa267c32
RamiKrispin <ramkrisp@umich.edu>
2024-03-31 20:49:58 -0700

    create a new docker build workflow: add base-r image and modify the dev

--- .devcontainer/build_base_r_image.sh
+++ .devcontainer/build_base_r_image.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Build the docker"
-
+image="rkrispin/base-r:4.3.3"
 docker build . -f Dockerfile.base \
                --progress=plain \
                --build-arg PROJECT_NAME="R Dev Env" \
                --build-arg VENV_NAME="R_ENV" \
                --build-arg R_VERSION_MAJOR=4 \
                --build-arg R_VERSION_MINOR=3 \
-               --build-arg R_VERSION_PATCH=1 \
+               --build-arg R_VERSION_PATCH=3 \
                 --build-arg DEBIAN_FRONTEND=noninteractive \
                  --build-arg CRAN_MIRROR="https://cran.rstudio.com/" \
-               --build-arg QUARTO_VER="1.3.450" \
-               -t rkrispin/vscode_r_dev:0.1.0
+               --build-arg QUARTO_VER="1.4.552" \
+               -t $image
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push rkrispin/vscode_r_dev:0.1.0
+docker push $image
 else
 echo "Docker build failed"
 fi
