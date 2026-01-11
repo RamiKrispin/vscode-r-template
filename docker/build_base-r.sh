55c106388e492d1dc6f2d152986e2f515b8b1959
RamiKrispin <ramkrisp@umich.edu>
2026-01-10 22:30:52 -0800

    updated the template

--- docker/build_base-r.sh
+++ docker/build_base-r.sh
@@ -8,22 +8,15 @@ image_label="baser"
 r_major=4
 r_minor=4
 r_patch=0
-quarto_ver="1.7.32"
+quarto_ver="1.8.26"
 python_ver="3.10"
 venv_name="r-dev"
 
-# Identify the CPU type (M1 vs Intel)
-if [[ $(uname -m) ==  "aarch64" ]] ; then
-  CPU="arm64"
-elif [[ $(uname -m) ==  "arm64" ]] ; then
-  CPU="arm64"
-else
-  CPU="amd64"
-fi
+
 
 # Setting the image name
 ver=${r_major}.${r_minor}.${r_patch}
-tag="${CPU}-0.0.1"
+tag="0.0.1"
 docker_file=Dockerfile.base-r
 image_name=$user_name/$image_label-$ver:$tag
 
@@ -32,6 +25,7 @@ echo "Image name: $image_name"
 # Build
 docker build . \
   -f $docker_file --progress=plain \
+  --platform linux/amd64,linux/arm64 \
   --build-arg PYTHON_VER=$python_ver \
   --build-arg R_VERSION_MAJOR=$r_major \
   --build-arg R_VERSION_MINOR=$r_minor \
--- docker/build_r-dev.sh
+++ docker/build_r-dev.sh
@@ -6,27 +6,17 @@ echo "Build the docker"
 user_name="rkrispin"
 image_label="r-dev"
 
-# Identify the CPU type (M1 vs Intel)
-if [[ $(uname -m) ==  "aarch64" ]] ; then
-  CPU="arm64"
-elif [[ $(uname -m) ==  "arm64" ]] ; then
-  CPU="arm64"
-else
-  CPU="amd64"
-fi
-
 # Setting the image name
 ver=0.0.1
-tag="${CPU}.${ver}"
 docker_file=Dockerfile.r-dev
-image_name=$user_name/$image_label:$tag
+image_name=$user_name/$image_label:$ver
 
 echo "Image name: $image_name"
 
 # Build
 docker build . \
   -f $docker_file --progress=plain \
-  --build-arg CPU=$CPU \
+  --platform linux/amd64,linux/arm64 \
    -t $image_name
 
 # Push
